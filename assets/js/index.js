2f47a481e6d6eb2b32084e66f4a98c27ad3db863
Source Wang <1154722015@qq.com>
2016-06-28 15:14:05 +0800

    scroll top 90

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,25 +7,25 @@ var counter = 0;
 setInterval(function(){
     counter += 1;
 
-    $("#chuan-title").fadeOut(2000, function() {
+    $("#chuan-title").fadeOut(1600, function() {
         switch (counter % 3) {
             case 0:
-                $("#chuan-title").html("To Be A Full Stack Engineer");
+                $("#chuan-title").html("No Pains No Gains");
                 break;
             case 1:
                 $("#chuan-title").html("Find Bugs And Resolve Them");
                 break;
             case 2:
-                $("#chuan-title").html("Feel At Home Please");
+                $("#chuan-title").html("More Skills More Efforts");
                 break;
             default:
-                $("#chuan-title").html("Thank Your For Your Visit");
+                $("#chuan-title").html("Thanks For Your Visit");
                 break;
         }
     });
     $("#chuan-title").fadeIn(1000);
 
-}, 2000);
+}, 1400);
 
 // Window Scroll
 var windowScroll = function () {
@@ -33,7 +33,7 @@ var windowScroll = function () {
 
         var scrollPos = $(this).scrollTop();
 
-        if ($(window).scrollTop() > 70) {
+        if ($(window).scrollTop() > 90) {
             $('#site-header-id').addClass('site-header-nav-scrolled');
         } else {
             $('#site-header-id').removeClass('site-header-nav-scrolled');
